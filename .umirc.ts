c29aa0fab47779dddd69c38c35123d78431f7e40
HerbertHe <30955264+HerbertHe@users.noreply.github.com>
2021-02-26 12:40:07 +0000

    Deploying to gh-pages from @ NucoTech/NucoTech.github.io@4aa8ef4a2c8689d09f984959008b7652ba2456fe 🚀

--- .umirc.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'dumi';
-
-export default defineConfig({
-  title: 'Nuco.Tech Open Source',
-  mode: 'site',
-  // more config: https://d.umijs.org/config
-});
--- README.md
+++
@@ -1,5 +0,0 @@
-# Nuco.Tech Open Source Page
-
-[![dumi](https://img.shields.io/badge/docs%20by-dumi-blue)](https://github.com/umijs/dumi)
-
-Nuco 开源页面
